bde088c17e01ed46773446ae3d9473042fc30b07
Christoffer Vikström <cvi@campanja.com>
2014-04-25 15:13:02 +0200

    Reenable a skipped test

--- test/ews_xml_SUITE.erl
+++ test/ews_xml_SUITE.erl
@@ -2,9 +2,7 @@
 -include_lib("common_test/include/ct.hrl").
 
 %% CT functions
--export([suite/0, groups/0, all/0,
-         init_per_testcase/2, end_per_testcase/2
-        ]).
+-export([suite/0, groups/0, all/0]).
 
 %% Tests
 -export([tag_with_multiple_namespaces/1,
@@ -22,14 +20,6 @@ groups() ->
 all() ->
     [{group, xml_test}].
 
-init_per_testcase(namespace_owerwriting, _Config) ->
-    {skip, fails};
-init_per_testcase(_TestCase, Config) ->
-    Config.
-
-end_per_testcase(_TestCase, Config) ->
-    Config.
-
 tag_with_multiple_namespaces(_Config) ->
     XMLString = "<a:test xmlns:a=\"ns-a\" xmlns:b=\"ns-b\" ><b:test2/></a:test>",
     Terms = ews_xml:decode(XMLString),
@@ -39,6 +29,4 @@ tag_with_multiple_namespaces(_Config) ->
 namespace_owerwriting(_Config) ->
     XMLString = "<test xmlns=\"a\" ><test2 xmlns=\"b\" /></test>",
     Terms = ews_xml:decode(XMLString),
-    Terms = [{{"a","test"},
-              [{{"xmlns",[]},"a"}],
-              [{{"b","test2"},[{{"xmlns",[]},"b"}],[]}]}].
+    Terms = [{{"a","test"},[],[{{"b","test2"},[],[]}]}].
