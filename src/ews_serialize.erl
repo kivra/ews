c7699ade952da2306ea373b329a2d84af866c6fa
Håkan Huss <huss@campanja.com>
2017-10-05 09:22:54 +0200

    Fix error in get_from_base

--- src/ews_serialize.erl
+++ src/ews_serialize.erl
@@ -299,11 +299,14 @@ has_inherited_type(Attributes, Tbl, TypeKey) ->
     end.
 
 get_from_base(Base, Tbl, TypeKey) ->
-    Candidates = [T || {_, T} <- ews_model:get_from_base(Base, Tbl),
-                       T#type.extends == TypeKey],
-    case Candidates of
-        [Type] ->
-            Type;
-        R when R == false; R == [] ->
-            false
+    case ews_model:get_from_base(Base, Tbl) of
+        false ->
+            false;
+        Candidates ->
+            case [T || {_, T} <- Candidates, T#type.extends == TypeKey] of
+                [Type] ->
+                    Type;
+                [] ->
+                    false
+            end
     end.
