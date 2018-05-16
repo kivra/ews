e44de41a35f42d1825b6d8930f5fe249bf1d3a4f
Håkan Huss <huss@campanja.com>
2018-05-16 11:12:35 +0200

    Update tests to handle multiple models change

--- test/ews_svc_SUITE.erl
+++ test/ews_svc_SUITE.erl
@@ -74,22 +74,22 @@ end_per_suite(_Config) ->
 
 % No model added tests ----------------------------------------------
 empty_lists_services(_Config) ->
-    {ok, []} = ews_svc:list_services().
+    {ok, []} = ews_svc:list_services(default).
 
 empty_list_types(_Config) ->
-    [] = ews_svc:list_types().
+    [] = ews_svc:list_types(default).
 
 empty_get_model(_Config) ->
-    undefined = ews_svc:get_model().
+    undefined = ews_svc:get_model(default).
 
 empty_list_full_clashes(_Config) ->
-    [] = ews_svc:list_full_clashes().
+    [] = ews_svc:list_full_clashes(default).
 
 empty_list_simple_clashes(_Config) ->
-    [] = ews_svc:list_simple_clashes().
+    [] = ews_svc:list_simple_clashes(default).
 
 empty_emit_model(_Config) ->
-    {error, no_model} = ews_svc:emit_model("test_file").
+    {error, no_model} = ews_svc:emit_model(default, "test_file").
 
 % One model added testing -------------------------------------------
 add_model(_Config) ->
@@ -107,25 +107,25 @@ add_model(_Config) ->
     %% with an already downloaded version
     Url = "https://adwords.google.com/api/adwords/cm/"
           "v201306/CampaignService?wsdl",
-    {ok,[{"CampaignService",3}]} = ews_svc:add_wsdl_url(Url),
+    {ok,[{"CampaignService",3}]} = ews_svc:add_wsdl_url(default, Url),
 
     true = meck:validate(lhttpc),
     meck:unload(lhttpc).
 
 
 one_model_list_services(_Config) ->
-    {ok, ["CampaignService"]} = ews_svc:list_services().
+    {ok, ["CampaignService"]} = ews_svc:list_services(default).
 
 one_model_list_service_ops(_Config) ->
     Ops = ["get","mutate","query"],
-    {ok, Ops} = ews_svc:list_service_ops("CampaignService").
+    {ok, Ops} = ews_svc:list_service_ops(default, "CampaignService").
 
 one_model_get_op_info(_Config) ->
     Service = "CampaignService",
     Ns = "https://adwords.google.com/api/adwords/cm/v201306",
     EndPoint = lists:concat([Ns, "/", Service]),
 
-    {ok, GetInfo} = ews_svc:get_op_info(Service, "get"),
+    {ok, GetInfo} = ews_svc:get_op_info(default, Service, "get"),
     "get" = proplists:get_value(name, GetInfo),
     [{"get", {Ns, "get"}}] = proplists:get_value(in, GetInfo),
     [{"RequestHeader", {Ns, "SoapHeader"}}] = proplists:get_value(in_hdr,
@@ -145,7 +145,7 @@ one_model_mutate_op_info(_Config) ->
     Ns = "https://adwords.google.com/api/adwords/cm/v201306",
     EndPoint = lists:concat([Ns, "/", Service]),
 
-    {ok, MutateInfo} = ews_svc:get_op_info(Service, "mutate"),
+    {ok, MutateInfo} = ews_svc:get_op_info(default, Service, "mutate"),
     "mutate" = proplists:get_value(name, MutateInfo),
     [{"mutate", {Ns, "mutate"}}] = proplists:get_value(in, MutateInfo),
     [{"RequestHeader", {Ns, "SoapHeader"}}] = proplists:get_value(in_hdr,
@@ -166,7 +166,7 @@ one_model_query_op_info(_Config) ->
     Ns = "https://adwords.google.com/api/adwords/cm/v201306",
     EndPoint = lists:concat([Ns, "/", Service]),
 
-    {ok, QueryInfo} = ews_svc:get_op_info(Service, "query"),
+    {ok, QueryInfo} = ews_svc:get_op_info(default, Service, "query"),
     "query" = proplists:get_value(name, QueryInfo),
     [{"query", {Ns, "query"}}] = proplists:get_value(in, QueryInfo),
     [{"RequestHeader", {Ns, "SoapHeader"}}] = proplists:get_value(in_hdr,
@@ -184,28 +184,28 @@ one_model_query_op_info(_Config) ->
 
 one_model_get_op(_Config) ->
     Service = "CampaignService",
-    {ok, Op} = ews_svc:get_op(Service, "get"),
+    {ok, Op} = ews_svc:get_op(default, Service, "get"),
     "get" = Op#op.name.
 
 one_model_mutate_op(_Config) ->
     Service = "CampaignService",
-    {ok, Op} = ews_svc:get_op(Service, "mutate"),
+    {ok, Op} = ews_svc:get_op(default, Service, "mutate"),
     "mutate" = Op#op.name.
 
 one_model_query_op(_Config) ->
     Service = "CampaignService",
-    {ok, Op} = ews_svc:get_op(Service, "query"),
+    {ok, Op} = ews_svc:get_op(default, Service, "query"),
     "query" = Op#op.name.
 
 one_model_list_types(_Config) ->
-    95 = length(ews_svc:list_types()).
+    95 = length(ews_svc:list_types(default)).
 
 one_model_emit_model(_Config) ->
     meck:new(ews_emit),
     meck:expect(ews_emit, model_to_file, 2,
                 fun(_Model, File) -> {model_emitted, File} end),
 
-    {model_emitted, "test_file"} = ews_svc:emit_model("test_file"),
+    {model_emitted, "test_file"} = ews_svc:emit_model(default, "test_file"),
     true = meck:validate(ews_emit),
     ok = meck:unload(ews_emit).
 
@@ -213,7 +213,7 @@ one_model_get_type(_Config) ->
     Ns = "https://adwords.google.com/api/adwords/cm/v201306",
     TypeKey = {Ns, "mutateResponse"},
 
-    Type = ews_svc:get_type(TypeKey),
+    Type = ews_svc:get_type(default, TypeKey),
 
     TypeKey = Type#type.qname,
     mutate_response = Type#type.alias,
@@ -237,7 +237,7 @@ one_model_call(_Config) ->
     meck:expect(ews_serialize, encode, 3, encoded),
     meck:expect(ews_serialize, decode, 3, [decoded]),
 
-    {ok, decoded} = ews_svc:call(Service, Op, HeaderParts, BodyParts),
+    {ok, decoded} = ews_svc:call(default, Service, Op, HeaderParts, BodyParts),
 
     [{Pid, {ews_soap, call, CallArgs}, {ok, {header, body}}}
     ] = meck:history(ews_soap),
